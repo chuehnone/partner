f33088d57e84e20fd0e45f331f994a024c32a8a2
ChinSheng <1897025+chuehnone@users.noreply.github.com>
2024-11-11 18:18:14 +0800

    feat(about): update about page style

--- src/screens/AboutScreen/AboutScreen.tsx
+++ src/screens/AboutScreen/AboutScreen.tsx
@@ -3,27 +3,37 @@ import React from 'react';
 import {observer} from 'mobx-react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import {View, FlatList, Text, StyleSheet} from 'react-native';
+import {Text, Card} from 'react-native-paper';
+import {View, FlatList} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-});
+import {styles} from './styles';
+import {useTheme} from '../../hooks';
 
 export const AboutScreen: React.FC = observer(() => {
+  const {colors} = useTheme();
+
+  const Item = ({title, value}: {title: string; value?: string}) => (
+    <Card
+      style={[
+        styles.card,
+        {backgroundColor: colors.surface},
+        {borderColor: colors.surfaceVariant},
+      ]}>
+      <Card.Content>
+        <View style={value ? styles.itemWithValue : styles.item}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.value}>{value}</Text>
+        </View>
+      </Card.Content>
+    </Card>
+  );
+
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
         <FlatList
-          data={[{key: '版本號：1.0.0'}, {key: '著作權聲明'}]}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+          data={[{key: '版本號', value: '1.0.0'}, {key: '著作權聲明'}]}
+          renderItem={({item}) => <Item title={item.key} value={item.value} />}
         />
       </View>
     </SafeAreaProvider>
---
+++ src/screens/AboutScreen/styles.ts
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  card: {
+    marginVertical: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+  },
+  item: {
+    padding: 10,
+    height: 40,
+  },
+  itemWithValue: {
+    padding: 10,
+    height: 60,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  value: {
+    fontSize: 14,
+    marginTop: 4,
+  },
+});
